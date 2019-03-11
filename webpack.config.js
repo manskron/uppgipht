ac420e26b48033bf334adc217a000babe4d197ca
Måns Kronkvist Brusbäck <mans.kronkvist@appliedtechnology.se>
2019-03-11 19:49:35 +0100

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./src/scripts/todo.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: "Uppgiphter", filename: "index.html", template: 'src/html/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    hot: true
+  }
+};
